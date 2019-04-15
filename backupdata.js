99a13ede624a42dc3b2c3221a1c117a3eb60cf29
lahlim <lassi_mustonen@hotmail.com>
2019-04-15 18:36:45 +0300

    päivitetty kurssit dataan

---
+++ backupdata.js
@@ -0,0 +1,222 @@
+var data = [
+    {
+        "koodi": "TJTP1121",
+        "nimi": "Palvelumuotoiluprojekti",
+        "laajuus": 2,
+        "arvosana": "H",
+        "pvm": "15.09.2017"
+    },
+
+    {
+        "koodi": "TJTP111",
+        "nimi": "Johdatus tietojärjestelmätieteeseen",
+        "laajuus": 3,
+        "arvosana": "H",
+        "pvm": "06.10.2017"
+    },
+
+    {
+        "koodi": "ITKP101",
+        "nimi": "Tietokone ja tietoverkot työvälineenä",
+        "laajuus": 2,
+        "arvosana": 4,
+        "pvm": "09.11.2017"
+    },
+
+    {
+        "koodi": "ITKP1130",
+        "nimi": "Johdatus sovelluskehitykseen",
+        "laajuus": 3,
+        "arvosana": "H",
+        "pvm": "05.12.2017"
+    },
+
+    {
+        "koodi": "TJTA113",
+        "nimi": "Tietojärjestelmien kehittäminen",
+        "laajuus": 5,
+        "arvosana": 4,
+        "pvm": "19.01.2018"
+    },
+
+    {
+        "koodi": "ITKP1011",
+        "nimi": "Web-julkaiseminen",
+        "laajuus": 2,
+        "arvosana": 5,
+        "pvm": "09.02.2018"
+    },
+
+    {
+        "koodi": "ITKY100",
+        "nimi": "Yliopisto-opiskelu ja opintojen suunnittelu",
+        "laajuus": 2,
+        "arvosana": "H",
+        "pvm": "22.02.2018"
+    },
+
+    {
+        "koodi": "ITKP2004",
+        "nimi": "Viestintä ja vuorovaikutus sosiaalisessa mediassa",
+        "laajuus": 3,
+        "arvosana": 4,
+        "pvm": "15.03.2018"
+    },
+
+    {
+        "koodi": "TIEP115",
+        "nimi": "Johdatus tietotekniikkaan",
+        "laajuus": 3,
+        "arvosana": 5,
+        "pvm": "21.03.2018"
+    },
+
+    {
+        "koodi": "XENI001",
+        "nimi": "Academic Reading",
+        "laajuus": 3,
+        "arvosana": "H",
+        "pvm": "30.04.2018"
+    },
+
+    {
+        "koodi": "XYHI001",
+        "nimi": "Viestintätaidot IT-alalla",
+        "laajuus": 2,
+        "arvosana": "H",
+        "pvm": "04.05.2018"
+    },
+
+    {
+        "koodi": "ITKA201",
+        "nimi": "Algoritmit 1",
+        "laajuus": 4,
+        "arvosana": 1,
+        "pvm": "18.05.2018"
+    },
+
+    {
+        "koodi": "ITKP104",
+        "nimi": "Tietoverkot",
+        "laajuus": 5,
+        "arvosana": 5,
+        "pvm": "23.05.2018"
+    },
+
+    {
+        "koodi": "ITKP102",
+        "nimi": "Ohjelmointi 1",
+        "laajuus": 6,
+        "arvosana": 4,
+        "pvm": "28.05.2018"
+    },
+
+    {
+        "koodi": "TIEP111",
+        "nimi": "Ohjelmointi 2",
+        "laajuus": 8,
+        "arvosana": 5,
+        "pvm": "01.08.2018"
+    },
+
+    {
+        "koodi": "KYBS5591",
+        "nimi": "Introduction to Hybrid Threats and Comprehensive Security (JSS28)",
+        "laajuus": 3,
+        "arvosana": "H",
+        "pvm": "17.08.2018"
+    },
+
+    {
+        "koodi": "TIES327",
+        "nimi": "Tietoverkkoturvallisuus",
+        "laajuus": 5,
+        "arvosana": 4,
+        "pvm": "31.08.2018"
+    },
+
+    {
+        "koodi": "YTTP1130",
+        "nimi": "Laskentatoimen perusteet",
+        "laajuus": 5,
+        "arvosana": 3,
+        "pvm": "19.10.2018"
+    },
+
+    {
+        "koodi": "YTTP1110",
+        "nimi": "Johtamisen ja johtajuuden perusteet",
+        "laajuus": 5,
+        "arvosana": 5,
+        "pvm": "02.11.2018"
+    },
+
+    {
+        "koodi": "YTTP1150",
+        "nimi": "Viestinnän johtamisen perusteet",
+        "laajuus": 5,
+        "arvosana": 4,
+        "pvm": "09.11.2018"
+    },
+
+    {
+        "koodi": "YTTP1120",
+        "nimi": "Markkinoinnin perusteet",
+        "laajuus": 5,
+        "arvosana": 2,
+        "pvm": "23.11.2018"
+    },
+
+    {
+        "koodi": "MATY010",
+        "nimi": "Matematiikan propedeuttinen kurssi",
+        "laajuus": 5,
+        "arvosana": 5,
+        "pvm": "28.11.2018"
+    },
+
+    {
+        "koodi": "TIEA2120",
+        "nimi": "Web-käyttöliittymien ohjelmointi",
+        "laajuus": 5,
+        "arvosana": 2,
+        "pvm": "03.12.2018"
+    },
+
+    {
+        "koodi": "TIEP1019",
+        "nimi": "Johdatus ohjelmistotekniikan matematiikkaan",
+        "laajuus": 3,
+        "arvosana": 2,
+        "pvm": "10.12.2018"
+    },
+
+    {
+        "koodi": "TIEP114",
+        "nimi": "Tietokoneen rakenne ja arkkitehtuuri",
+        "laajuus": 3,
+        "arvosana": 5,
+        "pvm": "20.12.2018"
+    },
+    {
+        "koodi": "ITKY2100",
+        "nimi": "Sparrausta akateemiseen opiskeluun",
+        "laajuus": 1,
+        "arvosana": "H",
+        "pvm": "06.03.2019"
+    },
+    {
+        "koodi": "TJTA104",
+        "nimi": "Ihmisen ja teknologian välinen vuorovaikutus",
+        "laajuus": 5,
+        "arvosana": 4,
+        "pvm": "07.03.2019"
+    },
+    {
+        "koodi": "ITKA204",
+        "nimi": "Tietokannat ja tiedonhallinnan perusteet",
+        "laajuus": 5,
+        "arvosana": 3,
+        "pvm": "13.03.2019"
+    }
+];
